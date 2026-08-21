6a2f8aa1abed4c4a68130f5dae05331da1fb5dd4
geczyakos <geczyakos@gmail.com>
2026-08-21 08:26:44 +0200

    feat: add account summary page headers

---
+++ src/utils/accountSummaryPageHeader.js
@@ -0,0 +1,8 @@
+// Builds the response headers for the "my account" summary page,
+// which shows the customer's upcoming bookings and payment history.
+
+export function buildAccountPageHeaders() {
+  return {
+    'Content-Type': 'text/html',
+  };
+}
